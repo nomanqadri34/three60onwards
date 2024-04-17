5497ee019c55b50c2bcaaf25917560b15a17cb05
Mohd noman Qadri <60312089+nomanqadri34@users.noreply.github.com>
2024-04-17 21:30:09 +0530

    Delete routes/Paymentroutes.js

--- routes/Paymentroutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import {
-  getRazorpayApiKey,
- allPayments,
-} from '../controllers/RazorpayController';
-const router = Router();
-router.route('/razorpay-key').get(isLoggedIn, getRazorpayApiKey);
-router.route('/').get(isLoggedIn, authorizeRoles('ADMIN'), allPayments);
-export default router;
